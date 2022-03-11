0b1c23ae3af3e8f6f9b368b8f827cf9ce8519964
Milenius <nstrohschen@gmail.com>
2022-03-11 19:42:54 +0100

    Added basic support for bootstrap tables. DictToMessages method still pending.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import os
+
 import spacy
 from tabulate import tabulate
 
@@ -11,7 +13,10 @@ from corpus import location
 import query
 import sys
 import wikicfpTable
+import gndTable
 import proceedingsDotComTable
+import pandas as pd
+
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -82,8 +87,174 @@ def limited_hardcoded_graph_addition_demo(neo_DB):
 # spacy needs to be ver. 3.2.1 and you may need to update the core if you update spacy
 # also watch
 # Press the green button in the gutter to run the script.
+
+
+from fb4.app import AppWrap
+from fb4.sqldb import db
+from sqlalchemy import Column
+import sqlalchemy.types as types
+from flask import redirect,render_template, request, flash, Markup, url_for, abort
+
+class Message(db.Model):
+    id = Column(types.Integer, primary_key=True)
+    text = Column(types.Text, nullable=False)
+    author = Column(types.String(100), nullable=False)
+    category = Column(types.String(100), nullable=False)
+    draft = Column(types.Boolean, default=False, nullable=False)
+    create_time = Column(types.Integer, nullable=False, unique=True)
+
+class Message2(db.Model):
+    acronym = Column(types.String(100), primary_key=True)
+    acronym2 = Column(types.String(100), nullable=False)
+    ordinal = Column(types.String(100), nullable=False)
+    year = Column(types.String(100), nullable=False)
+    country = Column(types.String(100), nullable=False)
+    dblp = Column(types.String(100), nullable=False)
+
+class HelloWeb(AppWrap):
+    '''
+    a sample web application
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        super().__init__(host='localhost')
+
+        AppWrap.addTemplatePath(self, "../templates")
+        db.init_app(self.app)
+        self.db = db
+
+        self.app.secret_key = 'dev'
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        self.app.config['BOOTSTRAP_BTN_STYLE'] = 'primary'
+        self.app.config['BOOTSTRAP_BTN_SIZE'] = 'sm'
+
+        @self.app.before_first_request
+        def before_first_request_func():
+            self.initDB()
+
+        @self.app.route('/table/<message_id>/view')
+        def view_message(message_id):
+            return self.message_view(message_id)
+
+        @self.app.route('/table/<message_id>/edit')
+        def edit_message(message_id):
+            return self.message_edit(message_id)
+
+        @self.app.route('/table/<message_id>/delete', methods=['POST'])
+        def delete_message(message_id):
+            return self.message_delete(message_id)
+
+        @self.app.route('/table')
+        def test_table():
+            return self.table()
+
+        @self.app.route('/')
+        def home():
+            return self.home()
+
+    def initDB(self,limit=20):
+        '''
+        initialize the database
+        '''
+        self.db.drop_all()
+        self.db.create_all()
+
+        self.initMessages(limit)
+
+    def message_delete(self, message_id):
+        message = Message.query.get(message_id)
+        if message:
+            db.session.delete(message)
+            db.session.commit()
+            return f'Message {message_id} has been deleted. Return to <a href="/table">table</a>.'
+        return f'Message {message_id} did not exist and could therefore not be deleted. Return to <a href="/table">table</a>.'
+
+    def message_edit(self, message_id):
+        message = Message.query.get(message_id)
+        if message:
+            message.draft = not message.draft
+            db.session.commit()
+            return f'Message {message_id} has been edited by toggling draft status. Return to <a href="/table">table</a>.'
+        return f'Message {message_id} did not exist and could therefore not be edited. Return to <a href="/table">table</a>.'
+
+    def message_view(self, message_id):
+        message = Message.query.get(message_id)
+        if message:
+            return f'Viewing {message_id} with text "{message.text}". Return to <a href="/table">table</a>.'
+        return f'Could not view message {message_id} as it does not exist. Return to <a href="/table">table</a>.'
+
+    def initMessages(self, limit=20):
+        '''
+        create an initial set of message with the given limit
+        Args:
+            limit(int): the number of messages to create
+        '''
+
+        for i in range(limit):
+            m = Message(
+                text='Test message {}'.format(i + 1),
+                author='Author {}'.format(i + 1),
+                category='Category {}'.format(i + 1),
+                create_time=4321 * (i + 1)
+            )
+            if i % 4:
+                m.draft = True
+            self.db.session.add(m)
+        self.db.session.commit()
+
+    def table(self):
+        '''
+        test table
+        '''
+        page = request.args.get('page', 1, type=int)
+        pagination = Message.query.paginate(page, per_page=10)
+        messages = pagination.items
+        titles = [('id', '#'), ('text', 'Message'), ('author', 'Author'), ('category', 'Category'), ('draft', 'Draft'),
+                  ('create_time', 'Create Time')]
+        return render_template('table.html', messages=messages, titles=None)
+
+
+
+    def home(self):
+        '''
+        render the home page of the HelloWeb application
+        '''
+        html = render_template("bootstrap.html", title="HelloWeb demo application",
+                               content="Welcome to the Flask + Bootstrap4 Demo web application", error=None)
+        return html
+
+
+helloWeb = HelloWeb()
+app = helloWeb.app
+
+def dictToMessages(ldict, app):
+    for x in range(1,len(ldict)):
+
+        keys = ldict[x].keys()
+        cur = ldict[x]['acronym']
+
+
+
+        m = Message(
+            text='Test message {}'.format(i + 1),
+            author='Author {}'.format(i + 1),
+            category='Category {}'.format(i + 1),
+            create_time=4321 * (i + 1)
+        )
+
+        app.db.session.add(m)
+        app.db.session.commit()
+
+
 if __name__ == '__main__':
 
+    parser = helloWeb.getParser("Flask + Bootstrap4 Demo Web Application")
+    args = parser.parse_args()
+    helloWeb.optionalDebug(args)
+    helloWeb.run(args)
 
     print("Have you done everything described in meo_4j.py? If so remove the print and following sys.exit()")
     sys.exit()
@@ -92,7 +263,7 @@ if __name__ == '__main__':
     # limited_hardcoded_graph_addition_demo(neo_DB)
     # neo_DB.close()
 
-    # sys.exit()
+    #sys.exit()
 
     ll = location.LocationLookup()  # initialize locationlookup
     nlp = spacy.load("en_core_web_trf")   # run "python -m spacy download en_core_web_trf" if it fails.
